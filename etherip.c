65dbfcfd5f3bf01f2f7eb546e18edc766d4a0ebb
tuna2134 <m-kikuchi@neody.ad.jp>
2025-11-23 00:09:32 +0000

    Add device cleanup in etherip_exit and initialize etherip_dev

--- etherip.c
+++ etherip.c
@@ -19,7 +19,7 @@ static netdev_tx_t etherip_xmit(struct sk_buff *skb, struct net_device *dev)
 {
     /* IPv6 Header, EtherIP Header追加 */
     struct sk_buff *new_skb;
-    struct iphdr *iph;
+    struct ipv6hdr *ip6h;
     dev_kfree_skb(skb);
 
     new_skb = alloc_skb(1500 + LL_RESERVED_SPACE(dev), GFP_ATOMIC);
@@ -30,10 +30,12 @@ static netdev_tx_t etherip_xmit(struct sk_buff *skb, struct net_device *dev)
 
     skb_put(new_skb, sizeof(struct ipv6hdr));
     ip6h = ipv6_hdr(new_skb);
-    ip6h->version     = 6;
-    ip6h->priority    = 0;
-    ip6h->flow_lbl    = 0;
-    ip6h->payload_len = htons(l2_len);
+    ip6h->version = 6;
+    ip6h->priority = 0;
+    ip6h->flow_lbl[0] = 0;
+    ip6h->flow_lbl[1] = 0;
+    ip6h->flow_lbl[2] = 0;
+    ip6h->payload_len = htons(0);
     ip6h->nexthdr     = 97;
     ip6h->hop_limit   = 64;
     
@@ -49,6 +51,8 @@ static const struct net_device_ops etherip_netdev_ops = {
 struct etherip_device {
     struct net_device *dev;
 };
+
+static struct net_device *etherip_dev;
 
 int setup_device(void) {
     struct etherip_device *priv;
@@ -69,6 +73,8 @@ int setup_device(void) {
         free_netdev(dev);
         return err;
     }
+
+    etherip_dev = dev;
     return 0;
 }
 
@@ -86,6 +92,11 @@ static int etherip_init(void) {
 
 /* アンインストール時に実行 */
 static void etherip_exit(void) {
+    if (etherip_dev) {
+        unregister_netdev(etherip_dev);
+        free_netdev(etherip_dev);
+        etherip_dev = NULL;
+    }
     printk(KERN_ALERT "Goodbye World!\n");
 }
 
